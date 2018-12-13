f5171d0077657affd891a813063504771d0c8e59
Tofunmi <jayteae94@gmail.com>
2018-12-13 08:28:13 +0100

    Update cost_function.py

--- cost_function.py
+++ cost_function.py
@@ -1,4 +1,5 @@
 from sigmoid import sigmoid
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
@@ -16,31 +17,20 @@ def cost_function(theta, X, y):
 
     Returns:
         l: The cost for logistic regression
-
     """
 
     l = None
-    #######################################################################
-    # TODO:                                                               #
-    # Compute and return the log-likelihood l of a particular choice of   #
-    # theta.                                                              #
-    #                                                                     #
-    #######################################################################
-    theta_transp = np.transpose(theta)
-    theta_x = np.multiply(theta_transp , X)
-    hyp = sigmoid(theta_x)
-    var1 = np.log(hyp)
-    var1_transp= np.transpose(var1)
-    var2 = np.multiply(var1_transp , y)
-    var3 = 1 - y
-    err = 1 - hyp
-    var4 = np.log(err)
-    var4_transp = np.transpose(var4)
-    var5 = np.multiply(var4_transp , var3)
-
-    l  = np.sum(-(var2 + var5))/X.__len__()
-
-    #######################################################################
-    #                         END OF YOUR CODE                            #
-    #######################################################################
+   
+    i = len(y[np.where(y==0)])    
+    j = len(y[np.where(y==1)])        
+    
+    m,n = X.shape
+    sigm = sigmoid(np.dot(theta.T, X.T)) #fine    
+    h_x = np.sum(sigm)/m     
+    a = np.log10(1 - h_x)           
+    var1 = i * a    
+    var2 = j  * np.log10(h_x)        
+    
+    l  = np.sum((var1 + var2))
+    
     return l
