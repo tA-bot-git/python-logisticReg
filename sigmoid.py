bcb61f696724be9fa0d09bc8d9e825d9f64fc195
Tofunmi <jayteae94@gmail.com>
2018-12-13 08:24:14 +0100

    Update sigmoid.py

--- sigmoid.py
+++ sigmoid.py
@@ -14,23 +14,9 @@ def sigmoid(z):
 
     Returns:
         g: An np.array of the same shape as z
-
     """
 
-    g = None
-    #######################################################################
-    # TODO:                                                               #
-    # Compute and return the sigmoid of z in g                            #
-    #######################################################################
-    list = [] #Initialize a list to store the sigmoid values
-    # for x in np.nditer(z): #iterate over the values of z
-    #     summ = 1 / (1 + np.exp(-x)) #store the values of each sigmoid calculation of x in z
-    #     list.append(summ) #append this sigmoid values to the list
-    # g = np.array(list) #convert the list into a numpy array with all the computed sigmoid values
-    g = 1 / (1 + np.exp(-z))
-    pass
-
-    #######################################################################
-    #                         END OF YOUR CODE                            #
-    #######################################################################
+    g = None        
+    g = 1 / (1 + np.exp(-z))    
+    
     return g
