6b128328d5a2a873228ff16616d04033fa23bee8
Tofunmi <jayteae94@gmail.com>
2018-12-13 08:24:49 +0100

    Update predict_function.py

--- predict_function.py
+++ predict_function.py
@@ -6,28 +6,14 @@ def predict_function(theta, X, y=None):
     """
     Compute predictions on X using the parameters theta. If y is provided
     computes and returns the accuracy of the classifier as well.
-
     """
 
     preds = None
-    accuracy = None
-    #######################################################################
-    # TODO:                                                               #
-    # Compute predictions on X using the parameters theta.                #
-    # If y is provided compute the accuracy of the classifier as well.    #
-    #                                                                     #
-    #######################################################################
+    accuracy = None    
     threshold = 0.5
     score = np.dot(X, theta)
     preds_1 = sigmoid(score)
-    preds = np.where(preds_1 >=threshold, 1, 0)
-    #if y=True:
-    accuracy = np.mean(y == preds)
-    #accuracy = (preds == y)
-
-    pass
-
-    #######################################################################
-    #                         END OF YOUR CODE                            #
-    #######################################################################
+    preds = np.where(preds_1 >=threshold, 1, 0)    
+    accuracy = np.mean(y == preds)        
+    
     return preds, accuracy
